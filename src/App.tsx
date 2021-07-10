c499171fc138ce2d6687935997208f97d6cecffa
Quang Mai <quangmainhat@gmail.com>
2021-07-10 13:53:09 +0700

    Fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const CANDIDATES_ENDPOINT =
+  'http://personio-fe-test.herokuapp.com/api/v1/candidates';
+
 function App() {
+  const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const response = await fetch(CANDIDATES_ENDPOINT);
+        const data = await response.json();
+
+        if (data) {
+          setData(data.data);
+        }
+        console.log('result', data);
+
+        // throw new Error('test error handling');
+      } catch (error) {
+        console.log('error', error);
+        setErrorMsg(error.message ? error.message : JSON.stringify(error));
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log('state', isLoading, data, errorMsg);
+
   return (
     <div className="App">
       <header className="App-header">
